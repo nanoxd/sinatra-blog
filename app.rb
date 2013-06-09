89db1c882ab63d8e7e29ae1b63231c252b93d010
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-09 10:35:57 -0700

    Move layout file to proper location, add validations, and stuff

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
+
+enable :sessions
 
 class Post < ActiveRecord::Base
-
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
 
 end
 
@@ -13,11 +18,30 @@ get "/" do
   erb :"posts/index"
 end
 
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post.'
+  else
+    redirect "posts/create", :error => 'Something went wrong. Try again.'
+  end
+end
+
+get "/posts/create" do
+  @title = "Create Post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
 get "/posts/:id" do
   @post = Post.find(params[:id])
   @title = @post.title
   erb :"posts/view"
 end
+
+
+
+
 
 helpers do
   def title

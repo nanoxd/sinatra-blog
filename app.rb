5cd7b193fc400acb1b82bfee6ae0ef816e0a3061
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-09 09:32:40 -0700

    Add routes and display posts

--- app.rb
+++ app.rb
@@ -4,4 +4,27 @@ require './environments'
 
 class Post < ActiveRecord::Base
 
+
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
+end
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome."
+    end
+  end
 end
